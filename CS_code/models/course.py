7a9754f0c59f352aca5405228b62f472fc00481b
Claire Shoemaker <clshoemaker@ursinus.edu>
2026-04-06 21:13:41 -0400

    Made a very rough outline of how the project will look

---
+++ CS_code/models/course.py
@@ -0,0 +1,3 @@
+"""
+course object goes here:
+"""
---
+++ CS_code/models/schedule.py
@@ -0,0 +1,5 @@
+"""
+schedule object goes here:
+    - this would basically be a list of courses
+    - the editSchedule and outputSchedule would be functions in this object
+"""
