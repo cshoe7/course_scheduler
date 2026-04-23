0c49a7079a65b5ce293ca29fb748e29e15187d12
Claire Shoemaker <clshoemaker@ursinus.edu>
2026-04-23 15:22:22 -0400

    baseline code done for rag pipline, still need to field errors and make sure everything works correctly

--- CS_code/services/fetch_courses.py
+++ CS_code/services/fetch_courses.py
@@ -9,6 +9,10 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
 #parses the pdf
 loader = PyMuPDFLoader("/Users/claireshoemaker/course_scheduler/CS_code/data/UCcc2025.pdf")
 #loads the context
@@ -30,9 +34,37 @@ embeddings = OllamaEmbeddings(
     headers={"Authorization": "Bearer " + os.environ["OLLAMA_API_KEY"]}
 )
 
+#where the rag info lives in vectorized form
+vectorstore = Chroma.from_documents(chunks, embeddings)
+#retrives the info from the vector store
+retriever = vectorstore.as_retriever()
+
+
+llm = ChatOllama(
+    model = "gpt-oss:120b",
+    base_url = "https://ollama.com",
+    headers={"Authorization": "Bearer " + os.environ["OLLAMA_API_KEY"]}
+)
+
+prompt = ChatPromptTemplate.from_template("""
+
+Answer the question based only on the following context: {context}
+
+Question: {question}
+
+""")
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+print(chain.invoke("What types of courses do I have to take as a computer science major?"))
+
 """
 TODO: 
 - set environment variable
-- set up vector store and retriever, then try to test everything
 - figure out where all of this rag setup code should go
 """
