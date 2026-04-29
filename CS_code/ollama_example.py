9d747b54d4904d3d4c29c2246eb704f22743cb8a
Alex Hickman <axhickman3@gmail.com>
2026-04-29 18:29:11 -0400

    Fix ollama_example.py to adhere to linter

--- CS_code/ollama_example.py
+++ CS_code/ollama_example.py
@@ -8,11 +8,11 @@ client = Client(
 )
 
 messages = [
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+    },
 ]
 
 for part in client.chat('gpt-oss:120b', messages=messages, stream=True):
-  print(part['message']['content'], end='', flush=True)
+    print(part['message']['content'], end='', flush=True)
