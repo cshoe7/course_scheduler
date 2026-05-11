9cf459571a917deece532a99427d6870035f3e6d
Claire Shoemaker <clshoemaker@ursinus.edu>
2026-05-10 21:28:45 -0400

    added a system prompt but chunks/similarity search aren't capturing the relevant sections of the catalog

--- CS_code/main.py
+++ CS_code/main.py
@@ -32,8 +32,8 @@ docs = loader.load()
 
 #sliding window of context
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000, #how many characters it looks at at once
-    chunk_overlap = 200 #charcaters that overlap in each chunk
+    chunk_size = 2000, #how many characters it looks at at once
+    chunk_overlap = 400 #charcaters that overlap in each chunk
 )
 
 #splits up the document into chunks
@@ -47,7 +47,22 @@ client = Client(
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 vectorstore = FAISS.from_documents(chunks, embeddings)
 
+prompt = """ 
+    You are an expert in the Ursinus College course catalog. Students will ask you questions about the courses offered at Ursinus college, 
+    and you will answer correctly and conciselly. You will not hallucinate any answers. You will cite the page of the course catalog you 
+    get your information from. If something is unclear, you will follow up with a clarifying question. If the The following are types of questions 
+    you may recieve and how you will answer them:
+
+    If the user asks for a list of courses that fulfill a reuirement, you will explain what the requiremnent is, then you will list the courses that
+    fulfill it. You should include a summary of the course description. List 10-15 courses as examples, then ask what the users interests are to help 
+    narrow down the options.
+
+
+ """
+
 print("Ready.")
+
+
 
 # --- Request schema ---
 class Message(BaseModel):
@@ -58,10 +73,17 @@ class Message(BaseModel):
 async def recommend(payload: Message):
 
     query = payload.message
-    retrieved = vectorstore.similarity_search(query, k=3)
+    retrieved = vectorstore.similarity_search(query, k=6)
     context = "\n\n".join([doc.page_content for doc in retrieved])
 
+    print(context)
+
+
     messages = [
+        {
+            "role": "system",
+            "content": prompt
+        },
         {
             "role": "user",
             "content": f"Use the following context to answer the question.\n\nContext:\n{context}\n\nQuestion: {query}"
